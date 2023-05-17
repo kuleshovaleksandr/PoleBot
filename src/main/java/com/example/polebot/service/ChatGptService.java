bab227c5373d26c16fec9f4afbf05cab9b6c7e2b
drevenb <drevenb@gmail.com>
2023-05-17 12:55:04 +0300

    getResponse is combined in ChatGptService

--- src/main/java/com/example/polebot/service/ChatGptService.java
+++ src/main/java/com/example/polebot/service/ChatGptService.java
@@ -1,8 +1,5 @@
 package com.example.polebot.service;
 
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.service.OpenAiService;
 import lombok.SneakyThrows;
 import org.apache.http.HttpHeaders;
 import org.springframework.stereotype.Service;
@@ -14,44 +11,24 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
-import java.time.Duration;
-import java.util.List;
+import java.util.Base64;
 
 @Service
 public class ChatGptService {
 
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "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";
     private static final String GPT_MODEL = "gpt-3.5-turbo";
     private static final String VOICE_MODEL = "whisper-1";
 
     @SneakyThrows
     public String getChatGptResponse(String prompt) {
         URL url = new URL("https://api.openai.com/v1/chat/completions");
-        StringBuilder response = new StringBuilder();
         String requestBody = "{\n" +
-                "  \"model\": \"gpt-3.5-turbo\",\n" +
+                "  \"model\": \"" + GPT_MODEL + "\",\n" +
                 "  \"messages\": [{\"role\": \"user\", \"content\": \"Tell me a joke about space\"}]\n" +
                 "}\n";
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
-        connection.setRequestProperty(HttpHeaders.ACCEPT, "application/json");
-        connection.setRequestProperty(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY);
-        connection.setDoOutput(true);
 
-        OutputStream os = connection.getOutputStream();
-        os.write(requestBody.getBytes());
-        os.flush();
-
-        try(BufferedReader responseReader = new BufferedReader(
-                new InputStreamReader(connection.getInputStream(), "utf-8"))) {
-            String responseLine;
-            while ((responseLine = responseReader.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            System.out.println(response);
-        }
-        return response.toString();
+        return getResponse(requestBody, url);
 
 //        OpenAiService service = new OpenAiService(API_KEY, Duration.ofSeconds(30));
 //        ChatCompletionRequest request = ChatCompletionRequest.builder()
@@ -66,28 +43,29 @@ public class ChatGptService {
 //        return builder.toString();
     }
 
-    public String voiceToText() {
-        return null;
-    }
-
     @SneakyThrows
     public String getTranscription() {
         URL url = new URL("https://api.openai.com/v1/audio/transcriptions");
         File file = new File("./data/voices/savedVoice.mp3");
-        StringBuilder response = new StringBuilder();
-
+        byte[] fileBytes = Files.readAllBytes(file.toPath());
+        String audioBase64 = Base64.getEncoder().encodeToString(fileBytes);
         String requestBody = "{\n"
-                + "  \"file\": \"" + "./data/voices/savedVoice.mp3" + "\",\n"
+                + "  \"file\": \"" + audioBase64 + "\",\n"
                 + "  \"model\": \"" + VOICE_MODEL + "\"\n"
                 + "}";
+        return getResponse(requestBody, url);
+    }
 
+    @SneakyThrows
+    private String getResponse(String requestBody, URL url) {
+        StringBuilder response = new StringBuilder();
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
 //        connection.setRequestProperty(HttpHeaders.ACCEPT, "application/json");
         connection.setRequestProperty(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY);
         connection.setDoOutput(true);
-        System.out.println(connection.getRequestProperties());
+        connection.setDoInput(true);
 //        try(OutputStream writer = connection.getOutputStream()) {
 //            byte[] input = requestBody.getBytes("utf-8");
 //            writer.write(input, 0, input.length);
@@ -96,6 +74,7 @@ public class ChatGptService {
         OutputStream os = connection.getOutputStream();
         os.write(requestBody.getBytes());
         os.flush();
+        os.close();
 
         try(BufferedReader responseReader = new BufferedReader(
                 new InputStreamReader(connection.getInputStream(), "utf-8"))) {
@@ -103,7 +82,6 @@ public class ChatGptService {
             while ((responseLine = responseReader.readLine()) != null) {
                 response.append(responseLine.trim());
             }
-            System.out.println(response);
         }
         return response.toString();
     }
